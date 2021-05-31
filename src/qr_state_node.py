552895748b04e6dff71ab47de0bd0c81b3f9eb28
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-05-31 15:33:42 -0400

    performance tweaks

--- src/qr_state_node.py
+++ src/qr_state_node.py
@@ -3,7 +3,13 @@ import rospy
 import time
 import sys
 
+sys.path.insert(1, '/home/turtlebot/.local/lib/python2.7/site-packages/cv2/')
+#__requires__="opencv-python==4.2.0.32"
+#import pkg_resources
+#pkg_resources.require("opencv-python==4.2.0.32")
 import cv2
+
+print('opencv version '+str(cv2.__version__))
 from cv_bridge import CvBridge
 from rospy.rostime import Duration
 from sensor_msgs.msg import Image
@@ -13,33 +19,41 @@ from qr_state_reader.srv import ReadEnvironment, ReadEnvironmentRequest, ReadEnv
 BRIDGE = CvBridge()
 DETECTOR = cv2.QRCodeDetector()
 
-MOST_RECENT_READING = ''
+IMAGE = None
 MOST_RECENT_TIME = 0
 
 def image_read(image):
-    cv_image = BRIDGE.imgmsg_to_cv2(image)
-    res = (DETECTOR.detectAndDecode(cv_image))
-    text, points, _ = res
-    if text is not None and len(text) != 0:
-        rospy.loginfo_throttle_identical(0.5, 'New image: '+str(text))
-        global MOST_RECENT_READING
-        MOST_RECENT_READING = text
-        global MOST_RECENT_TIME
-        MOST_RECENT_TIME = time.time()
+    global IMAGE
+    IMAGE = image
 
 def handle_read_env(req):
     req.empty  # input is empty, ignore
-    if (time.time() - MOST_RECENT_TIME) > 10:
-        rospy.loginfo("Requested information from too long ago!")
+    global IMAGE
+    if IMAGE is None:
         return ReadEnvironmentResponse(ReadEnvironmentResponse.NONE)
 
-    if MOST_RECENT_READING == 'CRAFTINGTABLE': return ReadEnvironmentResponse(ReadEnvironmentResponse.CRAFTING_TABLE)
-    if MOST_RECENT_READING == 'TREE1': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE1)
-    if MOST_RECENT_READING == 'TREE2': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE2)
-    if MOST_RECENT_READING == 'TREE3': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE3)
-    if MOST_RECENT_READING == 'TREE4': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE4)
-    if MOST_RECENT_READING == 'ROCK1': return ReadEnvironmentResponse(ReadEnvironmentResponse.ROCK1)
-    if MOST_RECENT_READING == 'ROCK2': return ReadEnvironmentResponse(ReadEnvironmentResponse.ROCK2)
+    most_recent_reading = ''
+    start_reads = time.time()
+    sample_timing = 2
+    while time.time() - start_reads < sample_timing:
+        cv_image = BRIDGE.imgmsg_to_cv2(IMAGE)
+        res = (DETECTOR.detectAndDecode(cv_image))
+        text, points, _ = res
+        if text is not None and len(text) != 0:
+            rospy.loginfo("qr text reads: "+str(text))
+            most_recent_reading = text
+            break
+#    if (time.time() - MOST_RECENT_TIME) > 10:
+#        rospy.loginfo("Requested information from too long ago!")
+#        return ReadEnvironmentResponse(ReadEnvironmentResponse.NONE)
+
+    if most_recent_reading == 'CRAFTINGTABLE': return ReadEnvironmentResponse(ReadEnvironmentResponse.CRAFTING_TABLE)
+    if most_recent_reading == 'TREE1': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE1)
+    if most_recent_reading == 'TREE2': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE2)
+    if most_recent_reading == 'TREE3': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE3)
+    if most_recent_reading == 'TREE4': return ReadEnvironmentResponse(ReadEnvironmentResponse.TREE4)
+    if most_recent_reading == 'ROCK1': return ReadEnvironmentResponse(ReadEnvironmentResponse.ROCK1)
+    if most_recent_reading == 'ROCK2': return ReadEnvironmentResponse(ReadEnvironmentResponse.ROCK2)
 
     rospy.loginfo("Reading didn't match any known inputs!")
     return ReadEnvironmentResponse(ReadEnvironmentResponse.NONE)
